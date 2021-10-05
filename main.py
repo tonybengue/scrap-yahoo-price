1f7c38c0b8d9b5c000c7f33a42422a41d58d99f2
Tony Bengué <tonybengue@hotmail.fr>
2021-10-05 03:33:27 +0200

    add gitignore file and make Color class exernal

--- main.py
+++ main.py
@@ -5,18 +5,7 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-# Color exemple
-#print(bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m' # end of string
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from colors import Colors
     
 def main():
     stock = 'TSLA'
@@ -36,7 +25,7 @@ def main():
         
         nbRequests += 1
         print(
-            'Request: ' + str(nbRequests) + ' for stock ' + bcolors.OKGREEN + stock + bcolors.ENDC, price, priceEarn
+            'Request: ' + str(nbRequests) + ' for stock ' + Colors.OKGREEN + stock + Colors.ENDC, price, priceEarn
         )
         time.sleep(timeToSleep)
 
